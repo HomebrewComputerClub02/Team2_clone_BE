a32542b42c4bc68252bd733e0a488900ba39da19
kuk329 <kuk329@cau.ac.kr>
2023-03-10 15:32:04 +0900

    initial commmit

---
+++ src/main/java/com/homebrewtify/demo/src/test/TestController.java
@@ -0,0 +1,26 @@
+package com.homebrewtify.demo.src.test;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/test")
+public class TestController {
+
+    @Autowired
+    public TestController(){}
+
+
+    @ResponseBody
+    @RequestMapping("")
+    public String getAll(){
+        System.out.println("테스트");
+
+        return "Success Test";
+    }
+
+}
