613b2efa1108febd2ea38fd4945e0abe18ffafc7
sok5188 <sok5188@gmail.com>
2023-03-19 17:39:52 +0900

    기본 writer csvwriter4로 변경

--- src/main/java/com/homebrewtify/demo/config/batch/FileReadJobConfig.java
+++ src/main/java/com/homebrewtify/demo/config/batch/FileReadJobConfig.java
@@ -20,23 +20,24 @@ public class FileReadJobConfig {
     //private final CsvWriter csvWriter;
     private final CsvWriter2 csvWriter2;
     //private final CsvWriter3 csvWriter3;
+    private final CsvWriter4 csvWriter4;
     private static final int chunkSize = 50000;
     //Job : 하나의 배치 작업 단위
     //하나의 Job안에는 여러 Step이 존재하고 Step안에 Reader,Writer등이 포함된다
     @Bean
     public Job csvFileItemReaderJob() {
-        return jobBuilderFactory.get("Job30")
+        return jobBuilderFactory.get("CsvToDBJob")
                 .start(csvFileItemReaderStep())
                 .build();
     }
 
     @Bean
     public Step csvFileItemReaderStep() {
-        return stepBuilderFactory.get("Step30")
+        return stepBuilderFactory.get("CsvToDBStep")
                 //CsvEntity를 Reader에서 읽어오고 CsvEntity를 writer에게 넘겨줄 것이다
                 .<CsvEntity, CsvEntity>chunk(chunkSize)
                 .reader(csvReader.csvFileItemReader())
-                .writer(csvWriter2).allowStartIfComplete(true)
+                .writer(csvWriter4).allowStartIfComplete(true) //배포 시 이거 없애야 함(아니면 서버 돌릴 때 마다 이 작업 반복)
                 .build();
     }
 }
