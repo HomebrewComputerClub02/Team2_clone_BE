d943fcda4d3ddd7054a4aeb99ba7d7419058235f
kuk329 <kuk329@cau.ac.kr>
2023-03-26 20:23:28 +0900

    비번 암호화 전

--- src/main/java/com/homebrewtify/demo/controller/HelloController.java
+++ src/main/java/com/homebrewtify/demo/controller/HelloController.java
@@ -1,13 +1,14 @@
 package com.homebrewtify.demo.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class HelloController {
 
     @GetMapping("/hello")
     public String hello(){
-        return "hello";
+        return "hello.html";
     }
 }
