5481b59bf3502b7c0d210d8fc03bb3aa6d749173
sok5188 <sok5188@gmail.com>
2023-03-30 11:33:06 +0900

    플리 삭제, 유저의 플리 조회, 플리 음악 조회, 플리에 노래 추가,삭제 기능 추가

--- src/main/java/com/homebrewtify/demo/controller/PlayListController.java
+++ src/main/java/com/homebrewtify/demo/controller/PlayListController.java
@@ -1,5 +1,6 @@
 package com.homebrewtify.demo.controller;
 
+import com.homebrewtify.demo.dto.MusicDto;
 import com.homebrewtify.demo.service.MusicService;
 import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
@@ -21,27 +22,43 @@ public class PlayListController {
         musicService.createPlayList(userId);
         return ResponseEntity.ok("PlayList is created");
     }
-    @Operation(summary = "플레이리스트 조회")
-    @GetMapping("/{playListId}")
-    public ResponseEntity<String> getUserPlayList(@PathVariable String playListId){
-        //userId를 JWT로 받아서
-        Long userId=2L;
-        //TODO : 내 플레이리스트 클릭 시 나오는 화면에 대한 정보 반환 기능 추가
-        musicService.getPlaylist(playListId);
-        return ResponseEntity.ok("PlayList is created");
-    }
+
     @Operation(summary = "플레이리스트 이름 변경")
     @PutMapping("")
     public ResponseEntity<String> updatePlayListName(@RequestParam String playListId, @RequestParam String name){
         musicService.renamePlaylist(playListId, name);
         return ResponseEntity.ok("PlayList is updated");
     }
+
+    @Operation(summary = "플레이리스트 삭제")
+    @DeleteMapping("/{playListId}")
+    public ResponseEntity<String> deletePlayList(@PathVariable String playListId){
+        musicService.deletePlaylist(playListId);
+        return ResponseEntity.ok("PlayList is deleted");
+    }
     @Operation(summary = "유저의 플레이리스트 목록 조회")
     @GetMapping("/getUserPlayList")
-    public ResponseEntity<String> getListOfPlaylist(){
+    public ResponseEntity<MusicDto.Result> getListOfPlaylist(){
         //userId를 JWT로 받아서
         Long userId=2L;
-        //TODO: 유저의 플레이리스트 목록 조회 후 플리 id, 이름 리턴하는 api
-        return ResponseEntity.ok("PlayList is updated");
+        return ResponseEntity.ok(musicService.getUserPlayList(userId));
+    }
+    @Operation(summary = "플레이리스트 조회", description = "플리 이름은 알아서..? type=유저이름 or homebrewtify")
+    @GetMapping("/{playListId}")
+    public ResponseEntity<MusicDto.Result> getUserPlayList(@PathVariable String playListId){
+        return ResponseEntity.ok(musicService.getMusicByPlayList(playListId));
+    }
+
+    @Operation(summary = "플레이리스트에 노래 추가하기")
+    @PostMapping("/music")
+    public ResponseEntity<String> addMusicToPlayList(@RequestParam String playListId, @RequestParam String musicId){
+        musicService.addMusicToPlayList(playListId, musicId);
+        return ResponseEntity.ok("Music is added successfully");
+    }
+    @Operation(summary = "플레이리스트에 노래 삭제하기")
+    @DeleteMapping("/music")
+    public ResponseEntity<String> deleteMusicToPlayList(@RequestParam String playListId, @RequestParam String musicId){
+        musicService.deleteMusicFromPlaylist(playListId, musicId);
+        return ResponseEntity.ok("Music is deleted successfully");
     }
 }
