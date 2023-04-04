89fcde8f4de91f93371716452984e5ab7feda2ee
sok5188 <sok5188@gmail.com>
2023-04-04 21:12:17 +0900

    미사용 코드 주석 처리(임시)

--- src/test/java/com/homebrewtify/demo/controller/PostsApiControllerTest.java
+++ src/test/java/com/homebrewtify/demo/controller/PostsApiControllerTest.java
@@ -1,60 +1,60 @@
-package com.homebrewtify.demo.controller;
-
-import com.homebrewtify.demo.test.domain.posts.Posts;
-import com.homebrewtify.demo.test.domain.posts.PostsRepository;
-import com.homebrewtify.demo.test.dto.PostsSaveRequestDto;
-import org.assertj.core.api.Assertions;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.boot.test.web.server.LocalServerPort;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-
-import java.util.List;
-
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-public class PostsApiControllerTest {
-
-    @LocalServerPort
-    private int port;
-
-    @Autowired
-    private TestRestTemplate restTemplate;
-
-    @Autowired
-    private PostsRepository postsRepository;
-
-    @AfterEach
-    public void tearDown() throws Exception{
-        postsRepository.deleteAll();
-    }
-
-    @Test
-    public void Posts_등록된다() throws Exception{
-        // given
-        String title = "title";
-        String content = "content";
-        PostsSaveRequestDto requestDto = PostsSaveRequestDto.builder()
-                .title(title)
-                .content(content)
-                .author("author22")
-                .build();
-
-        String url ="http://localhost:"+port+"/api/v1/posts";
-
-        // when
-        ResponseEntity<Long> responseEntity = restTemplate.postForEntity(url,requestDto,Long.class);
-
-        // then
-        Assertions.assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.OK);
-        Assertions.assertThat(responseEntity.getBody()).isGreaterThan(0L);
-
-        List<Posts> all = postsRepository.findAll();
-        Assertions.assertThat(all.get(0).getTitle()).isEqualTo(title);
-        Assertions.assertThat(all.get(0).getContent()).isEqualTo(content);
-
-    }
-}
+//package com.homebrewtify.demo.controller;
+//
+//import com.homebrewtify.demo.test.domain.posts.Posts;
+//import com.homebrewtify.demo.test.domain.posts.PostsRepository;
+//import com.homebrewtify.demo.test.dto.PostsSaveRequestDto;
+//import org.assertj.core.api.Assertions;
+//import org.junit.jupiter.api.AfterEach;
+//import org.junit.jupiter.api.Test;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.boot.test.web.client.TestRestTemplate;
+//import org.springframework.boot.test.web.server.LocalServerPort;
+//import org.springframework.http.HttpStatus;
+//import org.springframework.http.ResponseEntity;
+//
+//import java.util.List;
+//
+//@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+//public class PostsApiControllerTest {
+//
+//    @LocalServerPort
+//    private int port;
+//
+//    @Autowired
+//    private TestRestTemplate restTemplate;
+//
+//    @Autowired
+//    private PostsRepository postsRepository;
+//
+//    @AfterEach
+//    public void tearDown() throws Exception{
+//        postsRepository.deleteAll();
+//    }
+//
+//    @Test
+//    public void Posts_등록된다() throws Exception{
+//        // given
+//        String title = "title";
+//        String content = "content";
+//        PostsSaveRequestDto requestDto = PostsSaveRequestDto.builder()
+//                .title(title)
+//                .content(content)
+//                .author("author22")
+//                .build();
+//
+//        String url ="http://localhost:"+port+"/api/v1/posts";
+//
+//        // when
+//        ResponseEntity<Long> responseEntity = restTemplate.postForEntity(url,requestDto,Long.class);
+//
+//        // then
+//        Assertions.assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.OK);
+//        Assertions.assertThat(responseEntity.getBody()).isGreaterThan(0L);
+//
+//        List<Posts> all = postsRepository.findAll();
+//        Assertions.assertThat(all.get(0).getTitle()).isEqualTo(title);
+//        Assertions.assertThat(all.get(0).getContent()).isEqualTo(content);
+//
+//    }
+//}
--- src/test/java/com/homebrewtify/demo/domain/posts/PostsRepositoryTest.java
+++ src/test/java/com/homebrewtify/demo/domain/posts/PostsRepositoryTest.java
@@ -1,49 +1,49 @@
-package com.homebrewtify.demo.domain.posts;
-
-
-import com.homebrewtify.demo.test.domain.posts.Posts;
-import com.homebrewtify.demo.test.domain.posts.PostsRepository;
-import org.aspectj.lang.annotation.After;
-import org.assertj.core.api.Assertions;
-import org.junit.jupiter.api.AfterEach;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-
-import java.util.List;
-
-@SpringBootTest
-public class PostsRepositoryTest {
-
-    @Autowired
-    PostsRepository postsRepository;
-
-    @AfterEach // 각 테스트 실행후 실행
-    public void cleanup(){
-        postsRepository.deleteAll();
-    }
-
-    @Test
-    public void 게시글저장_불러오기(){
-        // given
-        String title = "테스트 게시글";
-        String content = "테스트 본문";
-
-        postsRepository.save(Posts.builder()
-                .title(title)
-                .content(content)
-                .author("alphabet@naver.com")
-                .build());
-
-        // when
-        List<Posts> postsList = postsRepository.findAll();
-
-        // then
-        Posts posts = postsList.get(0);
-        Assertions.assertThat(posts.getTitle()).isEqualTo(title);
-        Assertions.assertThat(posts.getContent()).isEqualTo(content);
-
-
-    }
-}
+//package com.homebrewtify.demo.domain.posts;
+//
+//
+//import com.homebrewtify.demo.test.domain.posts.Posts;
+//import com.homebrewtify.demo.test.domain.posts.PostsRepository;
+//import org.aspectj.lang.annotation.After;
+//import org.assertj.core.api.Assertions;
+//import org.junit.jupiter.api.AfterEach;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//import java.util.List;
+//
+//@SpringBootTest
+//public class PostsRepositoryTest {
+//
+//    @Autowired
+//    PostsRepository postsRepository;
+//
+//    @AfterEach // 각 테스트 실행후 실행
+//    public void cleanup(){
+//        postsRepository.deleteAll();
+//    }
+//
+//    @Test
+//    public void 게시글저장_불러오기(){
+//        // given
+//        String title = "테스트 게시글";
+//        String content = "테스트 본문";
+//
+//        postsRepository.save(Posts.builder()
+//                .title(title)
+//                .content(content)
+//                .author("alphabet@naver.com")
+//                .build());
+//
+//        // when
+//        List<Posts> postsList = postsRepository.findAll();
+//
+//        // then
+//        Posts posts = postsList.get(0);
+//        Assertions.assertThat(posts.getTitle()).isEqualTo(title);
+//        Assertions.assertThat(posts.getContent()).isEqualTo(content);
+//
+//
+//    }
+//}
